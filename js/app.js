a727c66bdb0717d95be0620c1f8e8b1900a05cb8
Tat-Obmetitsa <67114701+Tat-Obmetitsa@users.noreply.github.com>
2021-09-17 18:02:23 +0300

    add: banner desktop

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 // import './smth'
 import '../scss/main.scss'
+
+import 'slick-carousel';
+$('.slick').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: true
+});
