d4f77a6d684ad8cf72254c4f96771710b503b510
Tat-Obmetitsa <67114701+Tat-Obmetitsa@users.noreply.github.com>
2021-09-20 01:07:29 +0300

    add: burger menu in header

--- js/app.js
+++ js/app.js
@@ -8,5 +8,45 @@ $('.slick').slick({
     autoplay: true,
     autoplaySpeed: 2000,
     dots: true,
-    arrows: true
+    arrows: true,
+    lazyLoad: 'progressive',
+    mobileFirst: true,
+    // responsive: [{
+
+    //     breakpoint: 1024,
+    //     settings: {
+    //         slidesToShow: 1,
+    //         infinite: true
+    //     }
+
+    // }, {
+
+    //     breakpoint: 600,
+    //     settings: {
+    //         slidesToShow: 3,
+    //         dots: true
+    //     }
+
+    // }, {
+
+    //     breakpoint: 300,
+    //     settings: "unslick" // destroys slick
+
+    // }]
 });
+
+(() => {
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+
+    menuBtnRef.addEventListener("click", () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+        mobileMenuRef.classList.toggle("is-open");
+    });
+})();
+
