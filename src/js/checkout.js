e4e44499dc1ef23cb42941df5b9de7dea2f5ef72
Tat-Obmetitsa <67114701+Tat-Obmetitsa@users.noreply.github.com>
2021-10-03 12:13:41 +0300

    refactor: fix radio btn,add objects(buyer,payment)

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -22,10 +22,30 @@ import {
     regMonthYear,
     regZip,
     regCard,
-    regPhone
+    regPhone,
+    checkbox
 
 } from './refs'
 
+
+let buyer = {
+    name: "",
+    address: "",
+    contact: "",
+    city: "",
+    state: "",
+    zip: "",
+    delivery_date: "",
+    agreement: ''
+};
+let payment = {
+    method: "",
+    card_name: "",
+    card_num: "",
+    cvv: "",
+    exp_month: "",
+    exp_year: ""
+};
 
 // year and month select, short forms
 (() => {
@@ -80,26 +100,29 @@ cvvInput.oninput = function () {
 // validation  
 
 form.addEventListener('input', (e) => {
-    let el = e.target
-    let elValue = e.target.value
+    let el = e.target;
+    let elValue = e.target.value;
     elValue.trim();
 
+    // buyer
     if (el.classList.contains("name")) {
         elValue.replace(/[0-9]/g, '');
-        regName.test(elValue) ? el.setCustomValidity('') : el.setCustomValidity("Fill in your full name(min.2 words). It can have letters, - ' ");
+
+        regName.test(elValue) ? (el.setCustomValidity(''), buyer.name = elValue) : el.setCustomValidity("Fill in your full name(min.2 words). It can have letters, - ' ");
 
     } else if (el.classList.contains("address")) {
-        elValue.value !== '' && regName.test(elValue) ? el.setCustomValidity('') : el.setCustomValidity("Fill in the information");
+        elValue.value !== '' && regName.test(elValue) ? (el.setCustomValidity(''), buyer.address = elValue) : el.setCustomValidity("Fill in the information");
 
     } else if (el.classList.contains("city")) {
         elValue.replace(/[0-9]/g, '');
-        elValue.value !== '' && regName.test(elValue) ? el.setCustomValidity('') : el.setCustomValidity("Can contain only letters, - ' ");
+        elValue.value !== '' && regName.test(elValue) ? (el.setCustomValidity(''), buyer.city = elValue) : el.setCustomValidity("Can contain only letters, - ' ");
 
     } else if (el.classList.contains("contact")) {
         if (contact.value !== '' && regPhone.test(contact.value)) {
-            el.setCustomValidity('')
+            el.setCustomValidity('');
+            buyer.contact = elValue;
         } else if (contact.value.length > 17) {
-            elValue = elValue.slice(0, 17)
+            elValue = elValue.slice(0, 17);
         } else if (contact.value.length < 17) {
             el.setCustomValidity("Number is too short");
         }
@@ -107,63 +130,75 @@ form.addEventListener('input', (e) => {
     } else if (el.classList.contains("code")) {
         if (elValue !== '' && regZip.test(elValue)) {
             el.setCustomValidity('');
+            buyer.zip = elValue;
         } else if (elValue.length > 5) {
-            elValue = elValue.slice(0, 5)
+            elValue = elValue.slice(0, 5);
         }
         else if (!regZip.test(elValue)) {
             el.setCustomValidity("Must contain 5 digits");
         }
 
+    } else if (el.classList.contains("datepick")) {
+        buyer.delivery_date = elValue
+
+        // payment
     } else if (el.classList.contains("card-name")) {
         elValue.replace(/[0-9]/g, '');
-        elValue !== '' && regName.test(elValue) ? el.setCustomValidity('') : el.setCustomValidity("Fill in your full name(min.2 words). It can have letters, - ' ");
+        elValue !== '' && regName.test(elValue) ? (el.setCustomValidity(''), payment.card_name = elValue) : el.setCustomValidity("Fill in your full name(min.2 words). It can have letters, - ' ");
 
     } else if (el.classList.contains("card-number")) {
         if (elValue !== '' && regCard.test(elValue)) {
             numInput.setCustomValidity('');
-
+            payment.card_num = elValue;
         } else if (elValue.length > 19) {
-            elValue = elValue.slice(0, 19)
+            elValue = elValue.slice(0, 19);
         }
 
     } else if (el.classList.contains("cvv")) {
         if (elValue !== '' && regCvv.test(elValue)) {
             el.setCustomValidity('');
+            payment.cvv = elValue
         } else if (elValue.length > 3) {
-            elValue = elValue.slice(0, 3)
+            elValue = elValue.slice(0, 3);
         }
         else if (!regCvv.test(elValue)) {
             el.setCustomValidity("Must contain only 3 digits");
         }
 
-    } else if (el.classList.contains("month") || el.classList.contains("year")) {
-        elValue !== '' && regMonthYear.test(elValue) ? el.setCustomValidity('') : el.setCustomValidity("Select expiration month and year");
+    } else if (el.classList.contains("month")) {
+        elValue !== '' && regMonthYear.test(elValue) ? (el.setCustomValidity(''), payment.exp_month = elValue) : el.setCustomValidity("Select expiration month");
+
+    } else if (el.classList.contains("year")) {
+        elValue !== '' && regMonthYear.test(elValue) ? (el.setCustomValidity(''), payment.exp_year = elValue) : el.setCustomValidity("Select expiration year");
 
     };
+
+    radio.forEach(el => {
+        if (el.checked) {
+            payment.method = el.value
+        }
+    });
 
     // check if input === option in datalist
     for (const opt of stateOptions) {
         if (state.value === opt.value) {
             state.setCustomValidity("");
+            buyer.state = state.value
             break;
         } else {
             state.setCustomValidity("Choose your city from the list below");
         }
     }
 
+
 })
 
-// change valid\invalid for radio btn
 
-const updateValue = e => e.target.setAttribute('checked', 'checked');
-radio.forEach(el => {
-    el.setAttribute('checked', 'unchecked')
-    el.addEventListener('click', e => updateValue(e))
+checkBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    checkbox.checked ? buyer.agreement = true : buyer.agreement = false
+
 });
-
-// checkBtn.addEventListener('click', () => {
-
-// });
 
 
 // add mask on inputs
