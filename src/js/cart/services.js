09bb36568f05b7c0dc58e1b237f6b5bc2187b495
Tat-Obmetitsa <67114701+Tat-Obmetitsa@users.noreply.github.com>
2021-11-12 10:31:18 +0200

    fix styles and installment payment

--- src/js/cart/services.js
+++ src/js/cart/services.js
@@ -60,9 +60,7 @@ function addPaymentMethod(obj) {
         totalPrice.push(((Number(total) * (100 + Number(allSelects[index].value))) / 100).toFixed(2))
         allPrices[index].textContent = (Number(totalPrice[index]) / (Number(allSelects[index].value) + 1)).toFixed(2);
 
-        payments.installmentsPrice = {
-            paymentsNumber: allPaymentNumbers[index].textContent, paymentPrice: allPrices[index].textContent, paymentsDuration: `${Number(allPaymentNumbers[index].textContent)}`, bank: allBankNames[index].textContent, totalPrice: totalPrice[index]
-        }
+         
 
         $(function () {
             $(element).alwaysChange(function (val) {
@@ -86,7 +84,10 @@ function addPaymentMethod(obj) {
 
             if (document.querySelector(".discount-price") !== null) { document.querySelector(".discount-price").classList.add('v-hidden') }
             document.querySelector(".credit.section").classList.add("modal-hidden")
-            utils.setStorageItem('payments', payments)
+            payments.installmentsPrice = {
+            paymentsNumber: allPaymentNumbers[index].textContent, paymentPrice: allPrices[index].textContent, paymentsDuration: `${Number(allPaymentNumbers[index].textContent)}`, bank: allBankNames[index].textContent, totalPrice: totalPrice[index]
+        } 
+            utils.setStorageItem('payments', payments) 
 
         })
 
